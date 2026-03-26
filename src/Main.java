7fb82a0bfb997cb87bb38b17c4b7812a7bb1e6d9
faarazfar <125113140+xyeez@users.noreply.github.com>
2026-03-26 12:29:44 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Kos kos = new Kos("Kos Melati");
+        Kos kos = new Kos("Kos AHAS");
 
         Kamar kamar1 = new Kamar(1);
         Penyewa penyewa1 = new Penyewa("Budi");
